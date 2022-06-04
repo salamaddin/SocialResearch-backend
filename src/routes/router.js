326a06e54cfef802eb770c00ce2db7a099674ec4
salamaddin <salamaddinsk100@gmail.com>
2022-06-04 22:21:21 +0530

    make it better

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,9 @@
 const express=require('express')
 const app=express()
+const cors = require('cors')
 const parser = require('body-parser')
 app.use(parser.json())
+app.use(cors())
 
 app.use('/signup', require('../middlewares/UserSignup'))
 app.use('/login', require('../middlewares/UserLogin'))
